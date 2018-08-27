1e58c756abed2e86a5b1494f9d7a0e6cf9d49fb0
yoniblumenfeld <yonei06@gmail.com>
2018-08-27 18:31:01 +0300

    Added simulations

---
+++ main.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const yargs = require('yargs');
+const simulator = require('./simulator');
+let app = express();
+const argv = yargs
+.option('p',{
+    alias: 'port',
+    describe: 'port to start server on',
+    demand: 'false',
+    type: 'number',
+    default: 3000
+})
+.help()
+.argv;
+
+const port = argv.port;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+
+app.post('/simulate/fighters',(req,res)=>{
+    let ranks = simulator.calcFightersTotalRank(req.body);
+    let percentages = simulator.calcFightersWinningPercentages(ranks.fighterARank,ranks.fighterBRank);
+    res.json({ranks,percentages});
+});
+
+app.post('/simulate/fighters/fight',(req,res)=>{
+    let {fighterA,fighterB} = simulator.getFightersObjects(req.body);
+    console.log('Fighter A OBJECT: ',fighterA);
+    console.log('Fighter B Object: ',fighterB);
+    simulator.simulateHundredFights(fighterA,fighterB).then((winner)=>{
+        res.json(winner);        
+    });
+});
+
+app.get('/',(req,res)=>{
+    res.json({msg:'Fighter Simulator API'});
+});
+
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`);
+});
---
+++ simulator.js
@@ -0,0 +1,96 @@
+const martialArtPercentage = {
+    karate: {
+        stamina: 20,
+        speed: 30,
+        strength: 20,
+        technique: 30
+    },
+    muayThai: {
+        stamina: 25,
+        speed: 30,
+        strength: 20,
+        technique: 25
+    },
+    boxing: {
+        stamina: 25,
+        speed: 25,
+        strength: 25,
+        technique: 25
+    }
+};
+
+let calcFightersWinningPercentages= (fighterARank,fighterBRank) => {
+    let weakerFighterPercentage = (100-Math.abs(fighterARank-fighterBRank))/2,
+        strongerFighterPercentage = weakerFighterPercentage + Math.abs(fighterARank-fighterBRank);
+    
+    return fighterARank>fighterBRank ? {'fighterAPercentage':strongerFighterPercentage,'fighterBPercentage':weakerFighterPercentage} : 
+    {'fighterAPercentage':weakerFighterPercentage,'fighterBPercentage':strongerFighterPercentage};
+};
+
+let calcFightersTotalRank = ([fighterA,fighterB]) => {
+    let fighterARank = calcFighterTotalRank(fighterA);
+    let fighterBRank = calcFighterTotalRank(fighterB);
+    return {fighterARank,fighterBRank};
+};
+
+let calcFighterTotalRank = (fighter) => {
+    let art = fighter.stats.art,
+        stats = fighter.stats;
+    console.log(art,stats);
+    return stats.stamina/(100/martialArtPercentage[art].stamina)+
+    stats.speed/(100/martialArtPercentage[art].speed) +
+    stats.strength/(100/martialArtPercentage[art].strength) +
+    stats.technique/(100/martialArtPercentage[art].technique);
+};
+
+let getFightersObjects = ([fighterA,fighterB]) => {
+    let ranks = calcFightersTotalRank([fighterA,fighterB]);
+    let percentages = calcFightersWinningPercentages(ranks.fighterARank,ranks.fighterBRank);
+    return {
+            fighterA:{
+                        winPercentage:parseFloat(percentages.fighterAPercentage).toFixed(1),
+                        totalRank: ranks.fighterARank
+                    },
+            fighterB:{
+                        winPercentage:parseFloat(percentages.fighterBPercentage).toFixed(1),
+                        totalRank: ranks.fighterBRank
+                    }
+            };
+}
+
+let simulateSingleFight = (fighterA,fighterB) => {
+        let randomNumber = Math.floor((Math.random()*1000)+1),
+        fighterARange = fighterA.winPercentage*10;
+        return {whoWon:randomNumber < fighterARange ? {fighterA} : {fighterB}};
+};
+
+let simulateHundredFights = (fighterA,fighterB) => {
+    fighterA.winningCounter = 0,
+    fighterB.winningCounter = 0;
+    return new Promise((resolve,reject)=>{
+        for(let i=0;i<100 || fighterA.winningCounter > 50 || fighterB.winningCounter > 50;++i){
+            new Promise((resolve,reject) => {
+                resolve(simulateSingleFight(fighterA,fighterB));
+            }).then((winner)=>{
+                if(winner.whoWon.hasOwnProperty('fighterA')){
+                    ++fighterA.winningCounter;
+                }else{        
+                    ++fighterB.winningCounter;
+                }
+            });
+        }
+        resolve({fighterA,fighterB});
+    }).then((fightersObj)=>{
+        console.log('Fighter A winnings: ',fightersObj.fighterA.winningCounter,'\n Fighter B winnings: ',fightersObj.fighterB.winningCounter);
+        let [fighterA,fighterB] = [fightersObj.fighterA,fightersObj.fighterB]
+
+        return fighterA.winningCounter > fighterB.winningCounter ? {fighterA} : {fighterB};
+    });
+};
+module.exports = {
+    calcFightersTotalRank,
+    calcFightersWinningPercentages,
+    getFightersObjects,
+    simulateSingleFight,
+    simulateHundredFights
+};
